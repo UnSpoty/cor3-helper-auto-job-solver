cacb87535ea0269102f2b31f80700b2268350b8a
NonForYou <nah@mail.ru>
2026-05-18 15:52:39 +0700

    change version

--- src/shared/build-info.js
+++ src/shared/build-info.js
@@ -12,7 +12,7 @@
     const root = (typeof globalThis !== 'undefined') ? globalThis : self;
     root.COR3 = root.COR3 || {};
     root.COR3.buildInfo = {
-        commit: '4ed026e',
+        commit: '9cd05c9',
         date: '2026-05-18',
     };
 })();
